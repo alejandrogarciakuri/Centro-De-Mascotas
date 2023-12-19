687d54d982d29fb4e0d5ae4ef92c9abb8a95033b
Alejandro Garcia Kuri <15234404@modelo.edu.mx>
2023-12-19 10:45:06 -0600

    La clase Mascota ahora esta disponible
    En esta se incluyen las validaciones requeridas y los comportamientos de cada uno de sus métodos.

---
+++ Project/Modelos/Mascota.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Project.Enumeradores;
+
+namespace Project.Modelos
+{
+    public class Mascota
+    {
+
+        private static int contadorId = 1;
+
+        public string Id { get; }
+
+        public string Nombre { get; }
+
+        public int _edad;
+        public int Edad
+        {
+            get { return _edad; }
+            set
+            {
+                if (Edad <= 0)
+                {
+                    throw new Exception("La edad debe ser mayor a cero.");
+                    return;
+                }
+
+                _edad = value;
+            }
+        }
+
+        public Temperamento Temperamento { get; }
+
+        public Persona Dueño { get; set; }
+
+        public Mascota(string nombre, int edad, Temperamento temperamento)
+        {
+            Id = GenerarId();
+            Nombre = nombre;
+            Edad = edad;
+            Temperamento = temperamento;
+            Dueño = null;
+        }
+
+        public void HacerRuido()
+        {
+            //Falta por implementar el ruido dependiendo de la especie.
+        }
+
+        public void CambiarDueno(Persona nuevoDueno)
+        {
+            if (Dueño != null)
+            {
+                Console.WriteLine($"El {Id} ha cambiado su dueño a {nuevoDueno.Nombre}");
+            }
+            Dueño = nuevoDueno;
+        }
+
+        private string GenerarId()
+        {
+            return $"{this.GetType().Name}-{contadorId++}";
+        }
+    }
+}
