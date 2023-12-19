bd68a05fa5c34cb1f768af4fbd3edae0062fc734
JUANITEZ123 <148462093+JUANITEZ123@users.noreply.github.com>
2023-12-19 11:40:00 -0600

    Se pueden ver los comportamientos de Capibara
    Falta linkeo a los enums

---
+++ Project/Modelos/Capibara.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Modelos
+{
+    public class Capibara : Mascota
+    {
+        public Capibara(string nombre, int edad)
+            : base(nombre, edad, Temperamento.AMABLE)
+        {
+        }
+
+        public override void HacerRuido()
+        {
+            Console.WriteLine("cui cui");
+        }
+    }
+
+}
