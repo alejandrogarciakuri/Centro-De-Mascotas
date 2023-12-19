85bcc9818cae7d0779fc5d08cd772d5a22a1362c
JUANITEZ123 <148462093+JUANITEZ123@users.noreply.github.com>
2023-12-19 10:16:16 -0600

    Ahora ya se puede usar los valores iniciales de Perro
    Falta agregado de funciones

---
+++ Project/Modelos/Perro.cs
@@ -0,0 +1,19 @@
+﻿using Project.Enumeradores;
+using Project.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Modelos
+{
+    // Clase Perro
+    public class Perro : Mascota, IAcariciable
+    {
+        public Perro(string nombre, int edad, Temperamento temperamento)
+            : base(nombre, edad, temperamento)
+        {
+        }
+
+    }
