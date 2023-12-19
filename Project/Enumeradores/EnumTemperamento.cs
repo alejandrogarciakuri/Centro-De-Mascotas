12fa41e66499c120c4579547acef507d59aabb07
JUANITEZ123 <148462093+JUANITEZ123@users.noreply.github.com>
2023-12-19 08:42:27 -0600

    Ahora se puede utilizar el enumerador de Temperamento

---
+++ Project/Enumeradores/EnumTemperamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Enumeradores
+{
+    public enum Temperamento
+    {
+        Amable,
+        Nervioso,
+        Agresivo
+    }
+}
