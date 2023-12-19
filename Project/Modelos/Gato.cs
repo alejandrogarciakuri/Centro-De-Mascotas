df15e3d88b647f4b1e62a777b11aec130a6e3a1f
Alejandro Garcia Kuri <15234404@modelo.edu.mx>
2023-12-19 10:59:16 -0600

    Ahora se puede llamar la clase Gato
    Esta incluye los comportamientos del gato como Hacer Ruido, Ronronear, Rasguñar y Responder una caricia. Al igual se valida la edad de la mascota

---
+++ Project/Modelos/Gato.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Project.Enumeradores;
+using Project.Interfaces;
+
+namespace Project.Modelos
+{
+    public class Gato : Mascota, IAcariciable
+    {
+        private int _edad;
+        public int Edad
+        {
+            get { return _edad; }
+            set
+            {
+                if (Edad > 18)
+                {
+                    throw new Exception("La edad maxima de un gato es de 18 años.");
+                    return;
+                }
+
+                _edad = value;
+            }
+        }
+
+        public Gato(string nombre, int edad, Temperamento temperamento) : base(nombre, edad, temperamento)
+        {
+            Edad = edad;
+        }
+
+        public void HacerRuido()
+        {
+            Console.WriteLine("miau miau");
+        }
+
+        public void Ronronear()
+        {
+            Console.WriteLine("Ronronea");
+        }
+
+        public void Rasguñar()
+        {
+            Console.WriteLine("Rasguña");
+        }
+
+        public void ResponderACaricia()
+        {
+            if (Temperamento == Temperamento.Amable || Temperamento == Temperamento.Nervioso)
+            {
+                Ronronear();
+            }
+            else if (Temperamento == Temperamento.Agresivo)
+            {
+                Rasguñar();
+            }
+        }
+    }
+}
+
