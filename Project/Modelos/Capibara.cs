c8ea1942d116df29ed56b997afe59d5163a7d942
JUANITEZ123 <148462093+JUANITEZ123@users.noreply.github.com>
2023-12-19 11:56:07 -0600

    Ahora ya se puede ver el comportamiento de capibara
    Likeo a enums

--- Project/Modelos/Capibara.cs
+++ Project/Modelos/Capibara.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Project.Enumeradores;
+using Project.Interfaces;
 
 namespace Project.Modelos
 {
     public class Capibara : Mascota
     {
         public Capibara(string nombre, int edad)
-            : base(nombre, edad, Temperamento.AMABLE)
+            : base(nombre, edad, Temperamento.Amable)
         {
         }
 
